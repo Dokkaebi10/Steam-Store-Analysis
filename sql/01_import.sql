5404ba11dd6126b3d40de36f2398511d2427fb60
Daniel <daniel.leeseung@gmail.com>
2026-04-13 15:07:16 -0600

    removed sql import

--- sql/01_import.sql
+++
@@ -1,8 +0,0 @@
--- Step 1: staging table to hold raw JSON
-CREATE TABLE IF NOT EXISTS raw_steam_blob (data JSONB);
-
-CREATE TABLE IF NOT EXISTS raw_steam AS
-SELECT 
-    key AS app_id,
-    values AS DATA
-FROM raw_steam_blob, jsonb_each(data);
